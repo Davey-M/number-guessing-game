968408060824a5ca39b0c391756efb6db928b9b6
Davey-M <davey.meuer@gmail.com>
2022-02-03 13:51:58 -0600

    Formatted client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,33 +1,26 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  console.log("jquery is loaded!")
-  $('#submit-button').on('click', getNumbers);
-  
+	console.log("jquery is loaded!");
+	$("#submit-button").on("click", getNumbers);
 }
 
-function getNumbers(){
-  let heatherGuess = $('#heather-guess').val();
-  let leahGuess = $('#leah-guess').val();
-  let danGuess = $('#dan-guess').val();
-  let daveGuess = $('#dave-guess').val();
-  console.log('heather guess is', heatherGuess);
-  console.log('leah guess is', leahGuess);
-  console.log('dan guess is', danGuess);
-  console.log('dave guess is', daveGuess);
-  
-  let settings = {
-    method: 'POST',
-    url: '/guess',
-    data: {
-      inputParams: {
-        
-      }
-    }
+function getNumbers() {
+	let heatherGuess = $("#heather-guess").val();
+	let leahGuess = $("#leah-guess").val();
+	let danGuess = $("#dan-guess").val();
+	let daveGuess = $("#dave-guess").val();
+	console.log("heather guess is", heatherGuess);
+	console.log("leah guess is", leahGuess);
+	console.log("dan guess is", danGuess);
+	console.log("dave guess is", daveGuess);
 
-  }
-  $.ajax(settings)
+	let settings = {
+		method: "POST",
+		url: "/guess",
+		data: {
+			inputParams: {},
+		},
+	};
+	$.ajax(settings);
 }
-
-
-
