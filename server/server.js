b7efeaa26de88895b46e992df7aa7f0133549972
Davey-M <davey.meuer@gmail.com>
2022-02-03 11:44:32 -0600

    Created random number endpoint

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 3000;
 
+let randomNumber = getRandomNumber();
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 25) + 1;
+}
+
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -10,7 +16,10 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
-
+app.get('/newRandomNumber', (req, res) => {
+    randomNumber = getRandomNumber();
+    res.sendStatus(201);
+})
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
